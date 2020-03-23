ad325104161eb3802ebeb988b197bb2570c745b9
Pacal2 <i-jackie@hotmail.com>
2020-03-23 17:20:39 +0100

    Add project

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected Animator anim;
+    protected Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void JumpedOn()
+    {
+        anim.SetTrigger("Death");
+        rb.velocity = Vector2.zero;
+    }
+
+    private void Death()
+    {
+        Destroy(this.gameObject);
+    }
+}
